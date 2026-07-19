417626509e693c89f4dbf7cf3895f513d52ec2db
vhar <vhar@mail.ru>
2026-07-19 20:45:56 +0300

    feat(diagnostic keys): check quiz type on create actions

--- packages/Vhar/Quiz/src/Application/Commands/QuizDiagnosticKey/CreateDiagnosticKey/CreateDiagnosticKeyHandler.php
+++ packages/Vhar/Quiz/src/Application/Commands/QuizDiagnosticKey/CreateDiagnosticKey/CreateDiagnosticKeyHandler.php
@@ -3,8 +3,11 @@
 namespace Vhar\Quiz\Application\Commands\QuizDiagnosticKey\CreateDiagnosticKey;
 
 use Illuminate\Support\Facades\DB;
+use Vhar\Quiz\Application\Exceptions\InvalidQuizTypeException;
 use Vhar\Quiz\Application\Mappers\QuizDiagnosticKeyViewMapper;
 use Vhar\Quiz\Application\Views\QuizDiagnosticKeyView;
+use Vhar\Quiz\Enums\QuizTypeEnum;
+use Vhar\Quiz\Models\Quiz;
 use Vhar\Quiz\Models\QuizDiagnosticKey;
 
 /**
@@ -34,6 +37,14 @@ final readonly class CreateDiagnosticKeyHandler
      */
     public function handle(CreateDiagnosticKeyCommand $command): QuizDiagnosticKeyView
     {
+        /** @var Quiz $quiz */
+        $quiz = Quiz::query()->findOrFail($command->quizId);
+
+        // Бизнес-защита на запись: не даем привязать ключ к обычному квизу
+        if ($quiz->quiz_type !== QuizTypeEnum::DIAGNOSTIC) {
+            throw InvalidQuizTypeException::forDiagnosticKeys($quiz->quiz_type->value);
+        }
+
         return DB::transaction(function () use ($command) {
             /** @var QuizDiagnosticKey $key */
             $key = QuizDiagnosticKey::create([
